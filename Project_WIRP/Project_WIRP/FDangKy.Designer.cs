7259ceb85a56299d27241eeebc9360b529c4198d
luaan <luandophu8114@gmail.com>
2024-02-17 20:08:02 +0700

    Push first project

---
+++ Project_WIRP/Project_WIRP/FDangKy.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Project_WIRP
+{
+    partial class FDangKy
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtTaiKhoan = new System.Windows.Forms.TextBox();
+            this.txtMatKhau = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnDangNhap = new System.Windows.Forms.Button();
+            this.btnDangKy = new System.Windows.Forms.Button();
+            this.rdoUser = new System.Windows.Forms.RadioButton();
+            this.rdoTho = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(39, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(238, 383);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txtTaiKhoan
+            // 
+            this.txtTaiKhoan.Location = new System.Drawing.Point(322, 123);
+            this.txtTaiKhoan.Name = "txtTaiKhoan";
+            this.txtTaiKhoan.Size = new System.Drawing.Size(150, 22);
+            this.txtTaiKhoan.TabIndex = 1;
+            // 
+            // txtMatKhau
+            // 
+            this.txtMatKhau.Location = new System.Drawing.Point(322, 176);
+            this.txtMatKhau.Name = "txtMatKhau";
+            this.txtMatKhau.Size = new System.Drawing.Size(150, 22);
+            this.txtMatKhau.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(319, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Tài khoản";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(319, 157);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Mật khẩu";
+            // 
+            // btnDangNhap
+            // 
+            this.btnDangNhap.Location = new System.Drawing.Point(322, 249);
+            this.btnDangNhap.Name = "btnDangNhap";
+            this.btnDangNhap.Size = new System.Drawing.Size(127, 23);
+            this.btnDangNhap.TabIndex = 5;
+            this.btnDangNhap.Text = "Đăng nhập";
+            this.btnDangNhap.UseVisualStyleBackColor = true;
+            // 
+            // btnDangKy
+            // 
+            this.btnDangKy.Location = new System.Drawing.Point(322, 294);
+            this.btnDangKy.Name = "btnDangKy";
+            this.btnDangKy.Size = new System.Drawing.Size(127, 23);
+            this.btnDangKy.TabIndex = 6;
+            this.btnDangKy.Text = "Đăng ký";
+            this.btnDangKy.UseVisualStyleBackColor = true;
+            // 
+            // rdoUser
+            // 
+            this.rdoUser.AutoSize = true;
+            this.rdoUser.Location = new System.Drawing.Point(326, 223);
+            this.rdoUser.Name = "rdoUser";
+            this.rdoUser.Size = new System.Drawing.Size(57, 20);
+            this.rdoUser.TabIndex = 7;
+            this.rdoUser.TabStop = true;
+            this.rdoUser.Text = "User";
+            this.rdoUser.UseVisualStyleBackColor = true;
+            // 
+            // rdoTho
+            // 
+            this.rdoTho.AutoSize = true;
+            this.rdoTho.Location = new System.Drawing.Point(420, 223);
+            this.rdoTho.Name = "rdoTho";
+            this.rdoTho.Size = new System.Drawing.Size(52, 20);
+            this.rdoTho.TabIndex = 8;
+            this.rdoTho.TabStop = true;
+            this.rdoTho.Text = "Thợ";
+            this.rdoTho.UseVisualStyleBackColor = true;
+            // 
+            // FDangKy
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(553, 413);
+            this.Controls.Add(this.rdoTho);
+            this.Controls.Add(this.rdoUser);
+            this.Controls.Add(this.btnDangKy);
+            this.Controls.Add(this.btnDangNhap);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMatKhau);
+            this.Controls.Add(this.txtTaiKhoan);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "FDangKy";
+            this.Text = "FDangKy";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtTaiKhoan;
+        private System.Windows.Forms.TextBox txtMatKhau;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnDangNhap;
+        private System.Windows.Forms.Button btnDangKy;
+        private System.Windows.Forms.RadioButton rdoUser;
+        private System.Windows.Forms.RadioButton rdoTho;
+    }
+}
