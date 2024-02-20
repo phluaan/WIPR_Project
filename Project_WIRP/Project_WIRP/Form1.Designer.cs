009909d733bca932cb51a16b1297458e7e87b705
ChimSe <22110377@student.hcmute.edu.vn>
2024-02-20 21:46:56 +0700

    push new branch

--- Project_WIRP/Project_WIRP/Form1.Designer.cs
+++ Project_WIRP/Project_WIRP/Form1.Designer.cs
@@ -59,7 +59,7 @@
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "git ";
             this.ResumeLayout(false);
 
         }
